11c7bb9fe84fce84a40e17acfc982695eaaf3282
Ignas Veleckis <ignas.veleckis@tesonet.com>
2021-02-09 17:33:17 +0200

    correctly splitting given data into timeline matrix

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,32 +1,66 @@
 import React, { useEffect, useState } from "react";
+import Line from "./Line";
 
 const Timeline = ({ data }) => {
-  const [rows, setRows] = useState([]);
-  const get_rows = () => {
-    const rows_check = [];
-    for (let i in data) {
-      if (!rows_check.includes(data[i].group)) {
-        rows_check.push(data[i].group);
+  const [matrix, setMatrix] = useState([]);
+  const [splitInto, setSplitInto] = useState(12);
+
+  const create_global_group = (raw_data) => {
+    const all_groups = find_different_groups(raw_data);
+    const global_group = [];
+    for (let i in all_groups) {
+      const new_group = create_new_group(all_groups[i], raw_data);
+      global_group.push(new_group);
+    }
+    return global_group;
+  };
+
+  const find_different_groups = (array) => {
+    const different_group_count = [];
+    for (let i in array) {
+      if (!different_group_count.includes(array[i].group)) {
+        different_group_count.push(array[i].group);
       }
     }
-    setRows(rows_check);
+    return different_group_count;
+  };
+
+  const create_new_group = (group_name, data_for_extraction) => {
+    const new_group = [];
+    for (let j in data_for_extraction) {
+      if (group_name === data_for_extraction[j].group) {
+        new_group.push(data_for_extraction[j]);
+      }
+    }
+    return new_group;
   };
 
   useEffect(() => {
-    data && get_rows();
+    const global_group = create_global_group(data);
+    setMatrix(global_group);
     // eslint-disable-next-line
   }, [data]);
+
   return (
-    <div>
-      <div>DATE TIMELINE</div>
-      <div>
-        {rows.map((row, i) => (
-          <div key={Math.random() * i} className="flex">
-            <div>{row}</div>
-            <div>timeline</div>
-          </div>
-        ))}
-      </div>
+    <div className="w-full">
+      <select
+        onChange={(e) => {
+          setSplitInto(Number(e.target.value));
+        }}
+      >
+        <option value="12">12 Y</option>
+        <option value="30">30 M</option>
+        <option value="7">7 W</option>
+      </select>
+      <div className="flex justify-center p-2">Splitted into : {splitInto}</div>
+      {matrix.map((item) => (
+        <Line
+          key={Math.random()}
+          data={item}
+          title={item[0].group}
+          splitInto={splitInto}
+        />
+      ))}
     </div>
   );
 };
