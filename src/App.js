187352f0a8f45182a27fd66cc523291732ae79f4
Ignas Veleckis <ignas.veleckis@outlook.com>
2021-02-16 18:05:32 +0200

    'immer-upgrade'

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import TimelineN from './new-timeline/Timeline';
 function App() {
     return (
         <div className='h-full flex justify-center items-center flex-col'>
-            <div className='p-2 w-full'>
+            <h1 className='text-3xl font-bold pb-8 text-gray-700'>
+                Timeline Demo
+            </h1>
+            <div className='p-2 w-full max-w-4xl'>
                 <TimelineN timeline_data={data_for_testing} />
             </div>
         </div>
