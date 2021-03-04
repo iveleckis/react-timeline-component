42cfd02f31d295ac232116163897010639c06bb0
Ignas Veleckis <ignas.veleckis@tesonet.com>
2021-03-04 11:40:07 +0200

    removed layout

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Timeline from "./timeline/Timeline";
 
 function App() {
   return (
-    <div className="flex h-full w-full bg-black">
-      <div className="flex w-64 bg-gray-600 flex-shrink-0">HUGE MENU</div>
+    <div className="flex justify-center items-center h-full w-full">
       <Timeline timeline_data={data_for_testing} />
     </div>
   );
